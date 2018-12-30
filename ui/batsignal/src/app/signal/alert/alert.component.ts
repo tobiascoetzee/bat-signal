dfba44cc2ce5c69e83c0e4abda1a71d65bc2db1c
Tobias Coetzee <tcoetzee@gmail.com>
2018-12-30 16:27:30 -0500

    Add alert capture form

--- ui/batsignal/src/app/signal/alert/alert.component.ts
+++ ui/batsignal/src/app/signal/alert/alert.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-alert',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alert.component.css']
 })
 export class AlertComponent implements OnInit {
+  alertForm: FormGroup;
+  alertSent = false;
+  alertReference = '234-sds-232-dsd-233d';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.initForm();
   }
 
+  private initForm() {
+    this.alertSent = false;
+
+    this.alertForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      villain: new FormControl('Choose...'),
+      description: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.email),
+      phoneNumber: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10)
+      ])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.alertForm.value);
+    this.alertSent = true;
+  }
+
+  onCancel() {
+    this.initForm();
+  }
+
+  onNewAlert() {
+    this.initForm();
+  }
 }
